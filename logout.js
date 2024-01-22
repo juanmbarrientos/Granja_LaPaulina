c80f3722d376c616c36fdc3206a387cd515d9770
Juan Barrientos <109054384+juanmbarrientos@users.noreply.github.com>
2024-01-21 23:11:39 -0300

    Add files via upload

--- logout.js
+++ logout.js
@@ -1,13 +1,10 @@
 // logout.js
+// Evento click del botón de logout
+document.querySelector('.navbar img').addEventListener('click', logout);
 
 // Función para realizar el logout
 function logout() {
     // Borra los datos almacenados en localStorage
     localStorage.clear();
-
-    // Redirige a index.html
-    window.location.href = 'index.html';
 }
 
-// Evento click del botón de logout
-document.querySelector('.navbar img').addEventListener('click', logout);
